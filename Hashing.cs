c402220463a9c346d634922cb82cb916dda1c841
KuebV <rwthompson01@bvsd.org>
2021-10-06 11:36:39 -0600

    Initial commit

---
+++ Hashing.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hashbrowns
+{
+    public class Hashing
+    {
+        /// <summary>
+        /// Create the Hashing Constructor
+        /// Hashbrowns are perfect for a healthy breakfast balance
+        /// </summary>
+        /// <param name="text"></param>
+        public Hashing(string text)
+        {
+            RawText = text;
+        }
+
+        internal string RawText;
+
+        public string MD5Hash()
+        {
+            using (MD5 md5 = MD5.Create())
+            {
+                byte[] inputBytes = Encoding.ASCII.GetBytes(RawText);
+                byte[] hashBytes = md5.ComputeHash(inputBytes);
+
+                StringBuilder sb = new StringBuilder();
+                for (int i = 0; i < hashBytes.Length; i++)
+                {
+                    sb.Append(hashBytes[i].ToString("X2"));
+                }
+                return sb.ToString();
+            }
+        }
+
+        public string SHA1Hash(bool lowerCase = false)
+        {
+            using (SHA1Managed sha1 = new SHA1Managed())
+            {
+                var hash = sha1.ComputeHash(Encoding.UTF8.GetBytes(RawText));
+                var sb = new StringBuilder(hash.Length * 2);
+
+                foreach (byte b in hash)
+                {
+                    if (lowerCase)
+                        sb.Append(b.ToString("x2"));
+                    else
+                        sb.Append(b.ToString("X2"));
+                }
+
+                return sb.ToString();
+            }
+        }
+
+        public string SHA256Hash(bool lowerCase = false)
+        {
+            using (SHA256 sha256Hash = SHA256.Create())
+            {
+                // ComputeHash - returns byte array  
+                byte[] bytes = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(RawText));
+
+                StringBuilder builder = new StringBuilder();
+                for (int i = 0; i < bytes.Length; i++)
+                {
+                    
+                    if (lowerCase)
+                        builder.Append(bytes[i].ToString("x2"));
+                    else
+                        builder.Append(bytes[i].ToString("X2"));
+                }
+                return builder.ToString();
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Hashbrowns
+ Hashbrowns is a simple .DLL that includes all of .NET's supported Hashing Methods
